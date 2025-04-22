595fed27e30aebe0730f73d1dd7a16b62411e9d8
TheFoolBrave <santiago_bravo82211@elpoli.edu.co>
2025-04-22 14:30:39 -0500

    si

--- app/src/main/java/com/bravo/pastelapp/MainActivity.java
+++ app/src/main/java/com/bravo/pastelapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bravo.pastelapp;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        EditText pastelText = findViewById(R.id.pastel_text);
 
         
     }
