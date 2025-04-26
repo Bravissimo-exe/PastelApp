9a71741db944eb0f409c886479b192fccb56fbaa
TheFoolBrave <santiago_bravo82211@elpoli.edu.co>
2025-04-26 12:06:45 -0500

    ptm

--- app/src/main/java/com/bravo/pastelapp/MainActivity.java
+++ app/src/main/java/com/bravo/pastelapp/MainActivity.java
@@ -1,15 +1,32 @@
 package com.bravo.pastelapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import kotlin.ULong;
+
 public class MainActivity extends AppCompatActivity {
+
+    String CHANNEL_ID = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +34,38 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        EditText pastelText = findViewById(R.id.pastel_text);
+        Button buttonPastel = findViewById(R.id.pastelear_button);
 
-        
+        createNotificationChannel();
+
+        buttonPastel.setOnClickListener(v -> createNotification());
     }
+
+    private void createNotification(){
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.pastel_app_icon)
+                .setContentTitle("Pastel")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(findViewById(R.id.pastel_text)))
+                .build();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
 }
